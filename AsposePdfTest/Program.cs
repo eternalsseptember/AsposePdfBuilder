e066e32636616ac8421eb1217186267da6dc0d1e
Maria <eternalsseptember@gmail.com>
2020-05-29 22:08:00 +0300

    Some tables....

--- AsposePdfTest/Program.cs
+++ AsposePdfTest/Program.cs
@@ -23,23 +23,68 @@ namespace AsposePdfTest
 
         public decimal? ContractsCount { get; set; } = 759;
 
-        public IList<Characteristics> Characteristics { get; set; }
+        public IList<Characteristic> Characteristics { get; set; }
+
+        public IList<Classification> Classifications { get; set; }
+
+        public IList<SupplierOffer> SupplierOffers { get; set; }
 
         public string SkuModel { get; set; } = "Classic";
 
         public string Producer { get; set; } = "SVETOCOPY";
     }
 
-    public class Characteristics
+    public class Characteristic
     {
         public string Name { get; set; }
 
         public string Value { get; set; }
 
-        public Characteristics(string name, string value)
+        public Characteristic(string name, string value)
         {
             Name = name;
             Value = value;
+        }
+    }
+
+    public class Classification
+    {
+        public string Dictionary { get; set; }
+
+        public string Code { get; set; }
+
+        public Classification(string dictionary, string code)
+        {
+            Dictionary = dictionary;
+            Code = code;
+        }
+    }
+
+    public class SupplierOffer
+    {
+        public string Name { get; }
+
+        public string Inn { get; }
+
+        public string Offer { get; }
+
+        public string DeliveryTime { get; }
+
+        public string DeliveryRegion { get; }
+
+        public int Price { get; }
+
+        public int Vat { get; }
+
+        public SupplierOffer(string name, string inn, string offer, string deliveryTime, string deliveryRegion, int price, int vat)
+        {
+            Name = name;
+            Inn = inn;
+            Offer = offer;
+            DeliveryTime = deliveryTime;
+            DeliveryRegion = deliveryRegion;
+            Price = price;
+            Vat = vat;
         }
     }
 
@@ -67,12 +112,24 @@ namespace AsposePdfTest
 
             var model = new SkuPdfModel()
             {
-                Characteristics = new List<Characteristics>
+                Characteristics = new List<Characteristic>
                 {
-                    new Characteristics("Количество листов в пачке", "45 шт"),
-                    new Characteristics("Класс бумаги", "C"),
-                    new Characteristics("Плотность бумаги", "80 гр/м2"),
-                    new Characteristics("Страна происхождения", "Россия")
+                    new Characteristic("Количество листов в пачке", "45 шт"),
+                    new Characteristic("Класс бумаги", "C"),
+                    new Characteristic("Плотность бумаги", "80 гр/м2"),
+                    new Characteristic("Страна происхождения", "Россия")
+                },
+                Classifications = new List<Classification>
+                {
+                    new Classification("ОКПД2", "32.99.12.120 Ручки и маркеры с наконечником из фетра и прочих пористых материалов"),
+                    new Classification("КПГЗ", "01.15.05.03.04 Принадлежности для досок и флипчартов"),
+                    new Classification("КТРУ", "32.99.12.120-00000001 Маркер")
+                },
+                SupplierOffers = new List<SupplierOffer>
+                {
+                    new SupplierOffer("ООО \"ЯСТРЕБ\"", "5427713792", "1219511-21", "5-10 дней", "г Москва, обл Московская", 47, 20),
+                    new SupplierOffer("ИП ВЛАДИМИР", "4767898456", "4578548-45", "2-3 часа", "г Казань, Татарстан", 30, 20),
+                    new SupplierOffer("ООО \"ЖЕНЕРИК\"", "7894598723", "6578459-12", "1-2 недели", "г Москва, обл Московская", 50, 20)
                 }
             };
 
@@ -95,9 +152,135 @@ namespace AsposePdfTest
             var characteristicTable = CreateCharacteristicTable(model);
             page.Paragraphs.Add(characteristicTable);
 
+            // Classification Table
+            var classificationTable = CreateClassificationTable(model);
+            page.Paragraphs.Add(classificationTable);
+
+            // Classification Table
+            var supplierOffersTable = CreateSupplierOffersTable(model);
+            page.Paragraphs.Add(supplierOffersTable);
+
             
             using var resultStream = File.Open("D:\\WorkAsposeFiles\\template.pdf", FileMode.Create);
             pdfDocument.Save(resultStream, SaveFormat.Pdf);
+        }
+
+        private static Table CreateSupplierOffersTable(SkuPdfModel model)
+        {
+            return TableBuilder.Create(builder =>
+            {
+                builder.SetCellPaddings(5f);
+                builder.SetColumnWidths(3, "150 200 100");
+                builder.SetDefaultTextStyle();
+
+                builder.AddSingleCellRow(cellBuilder =>
+                {
+                    cellBuilder.Cell.Margin = new MarginInfo(0, 12, 0, 15);
+                    cellBuilder.AddHtml(fragmentBuilder => fragmentBuilder.AppendHeader("Предложения поставщиков"));
+                });
+
+                builder.AddRow(rowBuilder =>
+                {
+                    rowBuilder.Row.BackgroundColor = Color.FromRgb(231 / 255f, 239 / 255f, 247 / 255f);
+
+                    rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                    {
+                        htmlBuilder.AppendText("Поставщик");
+                    }));
+
+                    rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                    {
+                        htmlBuilder.AppendText("Условия поставки");
+                    }));
+
+                    rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                    {
+                        htmlBuilder.AppendText("Стоимость с НДС");
+                    }));
+                });
+
+                foreach (var supplierOffer in model.SupplierOffers)
+                {
+                    builder.AddRow(rowBuilder =>
+                    {
+                        rowBuilder.Row.Border = new BorderInfo(BorderSide.Top,
+                            new GraphInfo {Color = Color.Gray, LineWidth = 0.5f});
+
+                        rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                        {
+                            htmlBuilder
+                                .AppendBoldText(supplierOffer.Name)
+                                .AppendText($"ИНН: {supplierOffer.Inn}")
+                                .AppendText($"Оферта: №: {supplierOffer.Offer}");
+                        }));
+
+                        rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                        {
+                            htmlBuilder
+                                .AppendText($"Срок поставки: {supplierOffer.DeliveryTime}")
+                                .AppendText($"Регион поставки: {supplierOffer.DeliveryRegion}");
+                        }));
+
+                        rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                        {
+                            htmlBuilder
+                                .AppendHighlighted($"{supplierOffer.Price} ₽")
+                                .AppendText($"НДС: {supplierOffer.Vat}%");
+                        }));
+                    });
+                }
+            });
+        }
+
+        private static Table CreateClassificationTable(SkuPdfModel model)
+        {
+            return TableBuilder.Create(builder =>
+            {
+                builder.SetCellPaddings(5f);
+                builder.SetColumnWidths(2, "200 300");
+                builder.SetDefaultTextStyle();
+                //builder.AddBorders();
+
+                builder.AddSingleCellRow(cellBuilder =>
+                {
+                    cellBuilder.Cell.Margin = new MarginInfo(0, 12, 0, 15);
+                    cellBuilder.AddHtml(fragmentBuilder => fragmentBuilder.AppendHeader("Классификация"));
+                });
+
+                builder.AddRow(rowBuilder =>
+                {
+                    rowBuilder.Row.BackgroundColor = Color.FromRgb(231 / 255f, 239 / 255f, 247 / 255f);
+
+                    rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                    {
+                        htmlBuilder.AppendText("Справочник");
+                    }));
+
+                    rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                    {
+                        htmlBuilder.AppendText("Код");
+                    }));
+                });
+
+                foreach (var classification in model.Classifications)
+                {
+                    builder.AddRow(rowBuilder =>
+                    {
+                        rowBuilder.Row.Border = new BorderInfo(BorderSide.Top,
+                            new GraphInfo {Color = Color.Gray, LineWidth = 0.5f});
+
+                        rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                        {
+                            htmlBuilder.AppendText(classification.Dictionary);
+                        }));
+
+                        rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
+                        {
+                            htmlBuilder.AppendText(classification.Code);
+                        }));
+                    });
+                }
+            });
         }
 
         private static Table CreateCharacteristicTable(SkuPdfModel model)
@@ -107,21 +290,22 @@ namespace AsposePdfTest
                 builder.SetCellPaddings(5f);
                 builder.SetColumnWidths(2, "200 300");
                 builder.SetDefaultTextStyle();
-                builder.AddBorders();
+                //builder.AddBorders();
 
                 builder.AddSingleCellRow(cellBuilder =>
                 {
+                    cellBuilder.Cell.Margin = new MarginInfo(0, 0, 0, 10);
                     cellBuilder.AddHtml(fragmentBuilder => fragmentBuilder.AppendHeader("Характеристики"));
                 });
 
                 builder.AddSingleCellRow(cellBuilder =>
                 {
                     cellBuilder.AddHtml(fragmentBuilder =>
-                        {
-                            fragmentBuilder
-                                .AppendText("Модель: " + model.SkuModel)
-                                .AppendText("Производитель: " + model.Producer);
-                        });
+                    {
+                        fragmentBuilder
+                            .AppendText("Модель: " + model.SkuModel)
+                            .AppendText("Производитель: " + model.Producer);
+                    });
                 });
 
                 builder.AddRow(rowBuilder =>
@@ -143,6 +327,9 @@ namespace AsposePdfTest
                 {
                     builder.AddRow(rowBuilder =>
                     {
+                        rowBuilder.Row.Border = new BorderInfo(BorderSide.Top,
+                            new GraphInfo {Color = Color.Gray, LineWidth = 0.5f});
+
                         rowBuilder.AddCell(cellBuilder => cellBuilder.AddHtml(htmlBuilder =>
                         {
                             htmlBuilder.AppendText(characteristic.Name);
@@ -164,7 +351,7 @@ namespace AsposePdfTest
                 builder.SetCellPaddings(5f);
                 builder.SetColumnWidths(3, "150 150 150");
                 builder.SetDefaultTextStyle();
-                builder.AddBorders();
+                //builder.AddBorders();
 
                 builder.AddRow(rowBuilder =>
                 {
@@ -426,6 +613,13 @@ namespace AsposePdfTest
             return this;
         }
 
+        public HtmlFragmentBuilder AppendEmptyLine()
+        {
+            _lines.Add(string.Empty);
+
+            return this;
+        }
+
         public HtmlFragmentBuilder AppendHighlighted(object text) => AppendHighlighted(text.ToString());
 
         public HtmlFragmentBuilder AppendHighlighted(string text)
@@ -440,6 +634,13 @@ namespace AsposePdfTest
         public HtmlFragmentBuilder AppendHeader(string text)
         {
             _lines.Add($"<span style=\"font-size: 1.4em; font-weight: bold; color: #254b82;\">{text}</span>");
+
+            return this;
+        }
+
+        public HtmlFragmentBuilder AppendBoldText(string text)
+        {
+            _lines.Add($"<span style=\"font-weight: bold;\">{text}</span>");
 
             return this;
         }
